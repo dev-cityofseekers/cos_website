78c66636421e2b96d61c533a7f97258c67cda392
Luke Ziegler <zieglerluke1@gmail.com>
2024-01-24 16:29:32 +0100

    white weclome section, remove the word festival

--- cos_website/src/components/sections/Team.tsx
+++ cos_website/src/components/sections/Team.tsx
@@ -1,120 +1,126 @@
 import { stringify } from "querystring";
 
 function Team() {
-    const teamMembers = [
-      {
-        name: 'Luke/Liiav',
-        role: 'Vibe control & Website Developer',
-        description:
-          "Hi, I'm Luke, Spiritual seeker, Musician, Visionary, UX Designer/Frontend Dev, Event Organizer from Vienna. In COS 23 I felt a calling to unite and nourish a community that chooses love over fear, yeah baby. Im super excited for COS24.",
-        image: '/images/team/luke.jpg',
-      },
-      {
-        name: 'Freddyi',
-        role: 'Finance & Legal',
-        description:
-          '21 years old and I’m in my second year of studying nursing. Most of my spare time is used for traveling, exploring new places, or meeting people. I can’t wait to get to know more people at and through the festival and create lifelong memories.',
-        image: '/images/team/freddyi.png',
-      },
-      {
-        name: "Ela",
-        role: "Workshops & Marketing",
-        description: "28 years old who loves to be outside, especially in the mountains, hiking in summer and skiing in winter. Favorite quote: 'If I could choose where I was born I would choose a place that knows no fear But since this change is mine to live it would be nowhere else but here' - Bukahara",
-        image: '/images/team/ela.jpg',
-      },
-      {
-        name: 'Adalbert',
-        role: 'Tech & Infrastructure',
-        description:
-          "21 year old tech guy and crazy person that you probably will find running around, solving problems. Let's connect, deeptalk, party and create amazing memories :)",
-        image: '/images/team/adel.jpg',
-      },
-      {
-        name: 'Sören',
-        role: 'Marketing ',
-        description:
-          "Farmgrown Marketing student, that aspires to do some good and pet some alpacas. I'm making sure to get only the best people on- and in front of the stage.",
-        image: '/images/team/soeren_cut.jpg',
-      }, 
-      {
-        name: "Didi",
-        role: "Team management & Artists",
-        description: "Rule number one: no one is allowed to leave my house with an empty stomach. So basically I'm the 25-year-old Grandma to all of my friends.",
-        image: '/images/team/didi.jpg',
-      },
-      {
-        name: "Johar",
-        role: "Marketing & Social Media",
-        description: "Idealistic neurodivergent teacher/coach, content creator, musician & budding techie. In my free time... well, I'm asleep. All over Germany. Let's have coffee outside our comfort zones!",
-        image: '/images/team/johar.jpg',
-      },
-      {
-        name: "Autumn",
-        role: "Designer",
-        description: "Just your local Canadian living and working in Germany. Also an avid musician and painter, just trying to make cool things for a living.",
-        image: '/images/team/autumn.jpg',
-      },
-      {
-        name: 'ELIPSØ',
-        role: 'DJ',
-        description: 'ELIPSØ is responsible for providing quality music, throughout the evenings.\r\n Keeping energy at an all time high, this DJ knows what the crowd wants and delivers without fail.',
-        image: '/images/team/jordan.jpeg',
-      },
-      {
-        name: "Elias",
-        role: "Tech & Safety Management",
-        description: "25, a carpenter and an event technician from Germany. Currently living in Wiesbaden.",
-        image: '/images/team/elias.png',
-      },
-      {
-        name: "Áron",
-        role: "Media production",
-        description: "Your everyday creative with a physics degree. Chasing a dream in art and media. With a deep passion towards jazz, photography, and storytelling, I'm here to document our journey through the festival.",
-        image: '/images/team/Áron.jpg',
-      },
-      
-      // Add more team members as needed
-    ];
-  
-    return (
-      <div className="md:w-full lg:w-9/12 mx-auto pb-10">
-        {/* Introduction */}
-        <div className="text-center mb-8 mx-8">
-          <h2 className="font-omnes text-4xl pb-4">Meet Our Team</h2>
-          <p className="text-gray-600">
-            We are a passionate group of individuals who came together to make this festival a reality.
-          </p>
-          <img
-            src="/images/team/teamfoto.png"
-            alt="Group Photo"
-            className="mt-4 rounded-lg shadow-lg w-full h-auto mx-auto"
-          />
-        </div>
-  
-        {/* Team Members */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mx-8 ">
-          {teamMembers.map((member, index) => (
-            <div
-              key={index}
-              className="bg-white shadow-lg rounded-lg overflow-hidden">
-              <img
-                src={member.image}
-                alt={member.name}
-                className="w-full h-56 object-cover"
-              />
-              <div className="p-4">
-                <h3 className="text-xl font-semibold">{member.name}</h3>
-                <p className="text-gray-600 text-sm">{member.role}</p>
-                <p className="text-gray-700 text-sm mt-2">
-                  {member.description}
-                </p>
-              </div>
-            </div>
-          ))}
-        </div>
+  const teamMembers = [
+    {
+      name: "Luke/Liiav",
+      role: "Vibe control & Website Developer",
+      description:
+        "Hi, I'm Luke, Spiritual seeker, Musician, Visionary, UX Designer/Frontend Dev, Event Organizer from Vienna. In COS 23 I felt a calling to unite and nourish a community that chooses love over fear, yeah baby. Im super excited for COS24.",
+      image: "/images/team/luke.jpg",
+    },
+    {
+      name: "Freddyi",
+      role: "Finance & Legal",
+      description:
+        "21 years old and I’m in my second year of studying nursing. Most of my spare time is used for traveling, exploring new places, or meeting people. I can’t wait to get to know more people at and through the Event and create lifelong memories.",
+      image: "/images/team/freddyi.png",
+    },
+    {
+      name: "Ela",
+      role: "Workshops & Marketing",
+      description:
+        "28 years old who loves to be outside, especially in the mountains, hiking in summer and skiing in winter. Favorite quote: 'If I could choose where I was born I would choose a place that knows no fear But since this change is mine to live it would be nowhere else but here' - Bukahara",
+      image: "/images/team/ela.jpg",
+    },
+    {
+      name: "Adalbert",
+      role: "Tech & Infrastructure",
+      description:
+        "21 year old tech guy and crazy person that you probably will find running around, solving problems. Let's connect, deeptalk, party and create amazing memories :)",
+      image: "/images/team/adel.jpg",
+    },
+    {
+      name: "Sören",
+      role: "Marketing ",
+      description:
+        "Farmgrown Marketing student, that aspires to do some good and pet some alpacas. I'm making sure to get only the best people on- and in front of the stage.",
+      image: "/images/team/soeren_cut.jpg",
+    },
+    {
+      name: "Didi",
+      role: "Team management & Artists",
+      description:
+        "Rule number one: no one is allowed to leave my house with an empty stomach. So basically I'm the 25-year-old Grandma to all of my friends.",
+      image: "/images/team/didi.jpg",
+    },
+    {
+      name: "Johar",
+      role: "Marketing & Social Media",
+      description:
+        "Idealistic neurodivergent teacher/coach, content creator, musician & budding techie. In my free time... well, I'm asleep. All over Germany. Let's have coffee outside our comfort zones!",
+      image: "/images/team/johar.jpg",
+    },
+    {
+      name: "Autumn",
+      role: "Designer",
+      description:
+        "Just your local Canadian living and working in Germany. Also an avid musician and painter, just trying to make cool things for a living.",
+      image: "/images/team/autumn.jpg",
+    },
+    {
+      name: "ELIPSØ",
+      role: "DJ",
+      description:
+        "ELIPSØ is responsible for providing quality music, throughout the evenings.\r\n Keeping energy at an all time high, this DJ knows what the crowd wants and delivers without fail.",
+      image: "/images/team/jordan.jpeg",
+    },
+    {
+      name: "Elias",
+      role: "Tech & Safety Management",
+      description:
+        "25, a carpenter and an event technician from Germany. Currently living in Wiesbaden.",
+      image: "/images/team/elias.png",
+    },
+    {
+      name: "Áron",
+      role: "Media production",
+      description:
+        "Your everyday creative with a physics degree. Chasing a dream in art and media. With a deep passion towards jazz, photography, and storytelling, I'm here to document our journey through COS.",
+      image: "/images/team/Áron.jpg",
+    },
+
+    // Add more team members as needed
+  ];
+
+  return (
+    <div className="md:w-full lg:w-9/12 mx-auto pb-10">
+      {/* Introduction */}
+      <div className="text-center mb-8 mx-8">
+        <h2 className="font-omnes text-4xl pb-4">Meet Our Team</h2>
+        <p className="text-gray-600">
+          We are a passionate group of individuals who came together to make
+          this dream a reality.
+        </p>
+        <img
+          src="/images/team/teamfoto.png"
+          alt="Group Photo"
+          className="mt-4 rounded-lg shadow-lg w-full h-auto mx-auto"
+        />
       </div>
-    );
-  }
-  
-  export default Team;
-  
+
+      {/* Team Members */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mx-8 ">
+        {teamMembers.map((member, index) => (
+          <div
+            key={index}
+            className="bg-white shadow-lg rounded-lg overflow-hidden"
+          >
+            <img
+              src={member.image}
+              alt={member.name}
+              className="w-full h-56 object-cover"
+            />
+            <div className="p-4">
+              <h3 className="text-xl font-semibold">{member.name}</h3>
+              <p className="text-gray-600 text-sm">{member.role}</p>
+              <p className="text-gray-700 text-sm mt-2">{member.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Team;
--- cos_website/src/components/sections/Welcome.tsx
+++ cos_website/src/components/sections/Welcome.tsx
@@ -1,90 +1,24 @@
-
-/*
-function Welcome2() {
-  return (
-    <section
-      className="relative h-screen flex flex-col items-center justify-center bg-cover bg-center text-white"
-      style={{ backgroundImage: `url(${sunsetImage})` }}
-    >
-      <img src={yourLogo} alt="Festival Logo" className="w-40 h-40 mb-4" />
-
-      <nav className="bg-transparent mb-4">
-        <ul className="flex space-x-4">
-          <li>
-            <Link to="program" smooth={true} duration={500}>
-              Program
-            </Link>
-          </li>
-          <li>
-            <Link to="faq" smooth={true} duration={500}>
-              FAQ
-            </Link>
-          </li>
-       
-        </ul>
-      </nav>
-
-      <div className="text-center px-4">
-        <h1 className="text-4xl font-bold mb-2">
-          Come as a stranger, leave as a friend
-        </h1>
-        <p className="text-lg">Join us for an unforgettable experience!</p>
-      </div>
-    </section>
-  );
-
-
-<img
-        src="/images/logo_small.png"
-        className="w-1/4 absolute right-0 p-10"
-      />
-
-}*/
-
-function Welcome2() {
+function Welcome() {
   return (
     <div className="relative w-full h-screen">
-      
       <img
-        src="/images/hero-cut.jpg"
+        src="/images/welcome_sun_left_crop.JPG"
         alt="Hero"
         className="w-full h-full object-cover"
       />
-
-      <div className="absolute top-0 w-full h-full flex items-center justify-center">
-        <div>
-          <div className="text-cos-off-black font-omnes text-center text-6xl lg:text-8xl">
-            Come as a stranger,
-            <br /> leave as a friend.
-          </div>
-
-          <div className="text-cos-off-black font-omnes text-center text-4xl lg:text-6xl">
-            June 6-9 2024 <br />
-            Hameln, Germany
+      <div className="absolute top-0 w-full h-full flex items-center justify-center drop-shadow-lg">
+        <div className="w-4/12 ">
+          <img
+            src="/images/logo_white_transparent.png"
+            className="mx-auto pt-8"
+            alt="Logo_black"
+          />
+          <div className="text-white font-omnes text-center text-4xl lg:text-6xl">
+            June 6-9 2024
           </div>
         </div>
       </div>
     </div>
-  );
-}
-
-function Welcome() {
-  return (
-    <div className="relative w-full h-screen">
-      
-      <img
-        src="/images/hero-cut.jpg"
-        alt="Hero"
-        className="w-full h-full object-cover"
-      />
-<div className="absolute top-0 w-full h-full flex items-center justify-center">
-  <div className="w-4/12 ">
-      <img src="/images/logo_black.png" className="mx-auto"/>
-      <div className="text-cos-off-black font-omnes text-center text-4xl lg:text-6xl">
-            June 6-9 2024 
-          </div>
-      </div>
-    </div></div>
   );
 }
 
